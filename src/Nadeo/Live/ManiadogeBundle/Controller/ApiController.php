7d69f7270e0c59fa3320f5b630cdc411dc94de3d
Maxime Raoust <maximeraoust@gmail.com>
2014-11-26 11:28:05 +0100

    Forming with my doges

--- src/Nadeo/Live/ManiadogeBundle/Controller/ApiController.php
+++ src/Nadeo/Live/ManiadogeBundle/Controller/ApiController.php
@@ -3,9 +3,11 @@
 namespace Nadeo\Live\ManiadogeBundle\Controller;
 
 use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Post;
 use FOS\RestBundle\Controller\Annotations\View;
 use FOS\RestBundle\Controller\FOSRestController;
 use Nadeo\Live\ManiadogeBundle\Entity\Doge;
+use Nadeo\Live\ManiadogeBundle\Form\DogeType;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
@@ -24,12 +26,40 @@ class ApiController extends FOSRestController
 
     /**
      * @ApiDoc(description="very wow")
-     * @Get("/doges/{name}")
+     * @Get("/doges/{id}")
      * @View(template="ManiadogeBundle:Api:data.html.twig", templateVar="data")
      * @ParamConverter("doge", class="ManiadogeBundle:Doge")
      */
     public function getDogeAction(Doge $doge)
     {
         return $doge;
+    }
+
+    /**
+     * @Get("/doges/new")
+     * @View(template="ManiadogeBundle:Api:form.html.twig", templateVar="form")
+     */
+    public function newDogeAction()
+    {
+        return $this->createForm(new DogeType(), null, ['action' => $this->generateUrl('v1_api_post_doge')])
+                ;
+    }
+
+    /**
+     * @Post("/doges")
+     * @View(template="ManiadogeBundle:Api:data.html.twig", templateVar="data")
+     */
+    public function postDogeAction(\Symfony\Component\HttpFoundation\Request $request)
+    {
+        $form = $this->createForm(new DogeType())->handleRequest($request);
+        if($form->isValid())
+        {
+            $doge = $form->getData();
+            $this->getDoctrine()->getManager()->persist($doge);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->routeRedirectView('v1_api_get_doge', ['id'=>$doge->getId()] );
+        }
+        throw new \InvalidArgumentException($form->getErrors());
     }
 }
---
+++ src/Nadeo/Live/ManiadogeBundle/Form/DogeType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Nadeo\Live\ManiadogeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DogeType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('imageUrl')
+            ->add('submit', 'submit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Nadeo\Live\ManiadogeBundle\Entity\Doge'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'nadeo_live_maniadogebundle_doge';
+    }
+}

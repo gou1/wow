7e57436e5c605b78dceeb3ef137b9effdac58891
Maxime Raoust <maximeraoust@gmail.com>
2014-12-10 14:23:47 +0100

    Improved ConvertToHtmlResponse

--- api/src/AppBundle/EventListener/ConvertToHtmlResponse.php
+++ api/src/AppBundle/EventListener/ConvertToHtmlResponse.php
@@ -8,11 +8,6 @@ class ConvertToHtmlResponse
 {
     public function onKernelResponse(FilterResponseEvent $event)
     {
-
-        if($event->getRequest()->getPathInfo() == "/") {
-            return;
-        }
-
         if (!$event->isMasterRequest()) {
             return;
         }

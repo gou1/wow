b818aebc6d3218e12fabd6636e879f56c9366538
Maxime Raoust <maximeraoust@gmail.com>
2014-12-29 11:15:59 +0100

    Allow access to manialink/index_dev.php from 192.68.*

--- manialink/index_dev.php
+++ manialink/index_dev.php
@@ -4,7 +4,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || substr($_SERVER['REMOTE_ADDR'], 0, 8)=='192.168.' || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
@@ -14,4 +14,4 @@ require_once __DIR__.'/vendor/autoload.php';
 
 $request = Request::createFromGlobals();
 
-echo new App($request->getUriForPath('/../api/web/app_dev.php').'');
+echo new App($request->getUriForPath('/../../api/app_dev.php').'');
